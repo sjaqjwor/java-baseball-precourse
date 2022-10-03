a31545c5244b2500f701f14637c29aa52f57881e
lunatic <lsklsk45@naver.com>
2022-10-03 18:07:14 +0900

    refactor (RefereeResult) : HintConstant 변경
    update : 
       - 상수 안에 있는 공백 문자 제거

--- src/main/java/baseball/referee/HintConstant.java
+++ src/main/java/baseball/referee/HintConstant.java
@@ -2,6 +2,6 @@ package baseball.referee;
 
 public class HintConstant {
     public static final String NOTHING = "낫싱";
-    public static final String STRIKE = "%s 스트라이크 ";
-    public static final String BALL = "%s 볼 ";
+    public static final String STRIKE = "%s스트라이크";
+    public static final String BALL = "%s볼 ";
 }
